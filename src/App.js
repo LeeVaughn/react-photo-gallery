c00c6619558f846b53706a05592bcaf37bcb45e4
LeeVaughn <dlvprogramming@gmail.com>
2021-09-06 18:34:23 -0700

    Create initial App.js component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import Nav from './components/Nav';
+import SearchForm from './components/SearchForm';
+import PhotoContainer from './components/PhotoContainer';
+import NotFound from '/components./NotFound';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <Nav />
+          <Route component={ SearchForm } />
+
+          <Switch>
+            <Route exact path="/" render={ () => <Redirect to="puppies" /> } />
+            <Route path="/search/:query" render={ ({ match }) => <PhotoContainer data={ match.params.query } /> } />
+            <Route component={ NotFound } />
+          </Switch>
+    
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;

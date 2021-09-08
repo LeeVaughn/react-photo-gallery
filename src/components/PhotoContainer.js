715136f57514e7ec5fe3a290bb53f651f60d591a
LeeVaughn <dlvprogramming@gmail.com>
2021-09-08 15:26:05 -0700

    Add functionality to update state and pass/render data

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import apiKey from '../config'
+import apiKey from '../config';
+import Photos from './Photos';
 
 class PhotoContainer extends Component {
 
@@ -10,19 +11,36 @@ class PhotoContainer extends Component {
   }
 
   componentDidMount() {
-    this.performSearch("pizza");
+    this.performSearch(this.props.data);
   }
 
   performSearch(query) {
     axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&safe_search=1&in_gallery=&per_page=24&format=json&nojsoncallback=1`)
     .then(response => {
-      console.log(response)
+      console.log(response.data.photos)
+      this.setState({
+        results: response.data.photos.photo,
+        loading: false
+      })
     })
+    .catch(error => {
+      console.log("Error fetching data", error)
+    })
+    .finally(function() {
+      // always executed
+    });
   }
 
   render() {
     return (
-      <h1>React Gallery App</h1>
+      <div className="photo-container">
+        <h2>Results</h2>
+        <ul>
+          {
+            (this.state.loading) ? <p>Loading...</p> : <Photos results={ this.state.results } />
+          }
+        </ul>
+      </div>
     )
   }
 }

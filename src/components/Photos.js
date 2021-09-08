81cd47dc0da7b8747f155061d3d8ec0994d22868
LeeVaughn <dlvprogramming@gmail.com>
2021-09-08 16:11:50 -0700

    Add key attribute

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -8,8 +8,8 @@ function Photos(props) {
   if (results.length) {
     pics = results.map(pic => {
       return (
-        <li>
-          <img src={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} key={pic.id} alt="" />
+        <li key={`${pic.id}`}>
+          <img src={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt="" />
         </li>
       );
     });

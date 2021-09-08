af9fa868d325f59ab2cf4c3f7dcb28b2e93527e8
LeeVaughn <dlvprogramming@gmail.com>
2021-09-08 16:08:13 -0700

    Update nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,9 @@ function Nav() {
   return (
     <nav className="main-nav">
       <ul>
-        <li><NavLink to="/puppies">Puppies</NavLink></li>
-        <li><NavLink to="/kittens">Kittens</NavLink></li>
-        <li><NavLink to="/surfing">Surfing</NavLink></li>
+        <li><NavLink to="/search/puppies">Puppies</NavLink></li>
+        <li><NavLink to="/search/kittens">Kittens</NavLink></li>
+        <li><NavLink to="/search/surfing">Surfing</NavLink></li>
       </ul>
     </nav>
   )

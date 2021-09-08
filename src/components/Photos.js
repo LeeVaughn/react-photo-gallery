69534b8afc925a62a438c9417ec638c0b4702ace
LeeVaughn <dlvprogramming@gmail.com>
2021-09-08 15:25:20 -0700

    Add initial component

---
+++ src/components/Photos.js
@@ -0,0 +1,21 @@
+function Photos(props) {
+  console.log("props", props)
+  const results = props.results;
+  let pics;
+
+  if (results.length) {
+    pics = results.map(pic => {
+      return (
+        <li>
+          <img src={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} key={pic.id} alt="" />
+        </li>
+      );
+    });
+  }
+
+  return (
+    pics
+  );
+}
+
+export default Photos;

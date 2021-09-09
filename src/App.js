a523fd888565d704768af9b32be9927f0aecc322
LeeVaughn <dlvprogramming@gmail.com>
2021-09-08 17:43:15 -0700

    Add individual route paths to match nav buttons

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
 
           <Switch>
             <Route exact path="/" render={ () => <Redirect to="/search/dogs%20surfing" /> } />
+            <Route path={ ["/puppies", "/kittens", "/surfing"] } render={ ({ match }) => <PhotoContainer data={ match.path.substring(1) } /> } />
             <Route path="/search/:query" render={ ({ match }) => <PhotoContainer data={ match.params.query } /> } />
             <Route component={ NotFound } />
           </Switch>

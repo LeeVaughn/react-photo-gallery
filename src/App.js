7c3ae50e0812d2774a3084869237afbea6633dcb
LeeVaughn <dlvprogramming@gmail.com>
2021-09-08 18:01:36 -0700

    Add comment to routing

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
 
           <Switch>
             <Route exact path="/" render={ () => <Redirect to="/search/dogs%20surfing" /> } />
+            {/*{ removes "/" from path so that it can be used in the component to fetch data }*/}
             <Route path={ ["/puppies", "/kittens", "/surfing"] } render={ ({ match }) => <PhotoContainer data={ match.path.substring(1) } /> } />
             <Route path="/search/:query" render={ ({ match }) => <PhotoContainer data={ match.params.query } /> } />
             <Route component={ NotFound } />
